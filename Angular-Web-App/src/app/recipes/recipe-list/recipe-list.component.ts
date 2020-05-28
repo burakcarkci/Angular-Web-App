329a7c6165f7791f508f4ce5b53c28b98dcff9d4
Burak <burakcarkci@hotmail.com>
2020-05-27 21:32:33 -0400

    Added new functionalities

--- Angular-Web-App/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular-Web-App/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +6,9 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a test', 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg')
   ];
@@ -13,6 +16,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  OnRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
